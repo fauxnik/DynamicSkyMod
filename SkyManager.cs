b90944159c4a826824550d49d82ef131e644b3e1
CybexGS <68516763+CybexGS@users.noreply.github.com>
2020-08-07 07:10:35 +0200

    Add files via upload
    Added a few bits of debug code and minor reorganisations

--- SkyManager.cs
+++ SkyManager.cs
@@ -41,35 +41,52 @@ namespace ProceduralSkyMod
 		public Material StarMaterial { get; set; }
 		public Material SkyMaterial { get; set; }
 		public Material CloudMaterial { get; set; }
+		public Material MoonMaterial { get; set; }
 
 		public Transform SkyCam { get; set; }
 		public Transform CloudPlane { get; set; }
-		public Transform EnvCam { get; set; }
+		public Transform ClearCam { get; set; }
 
 		void Start ()
 		{
 			defaultFog = RenderSettings.fogColor;
-			nightFog = new Color(defaultFog.r * 0.1f, defaultFog.g * 0.1f, defaultFog.b * 0.1f, 1f);
+			nightFog = new Color(defaultFog.r * 0.05f, defaultFog.g * 0.05f, defaultFog.b * 0.05f, 1f);
+
+			CloudMaterial.SetFloat("_CloudSpeed", 0.03f);
+			StarMaterial.SetFloat("_Exposure", 2.0f);
 
 			StartCoroutine(CloudChanger());
 		}
 
 		void Update ()
 		{
-			skyboxNight.Rotate(Vector3.forward, 1f * Time.deltaTime, Space.Self);
-			moonBillboard.Rotate(Vector3.forward, -0.9f * Time.deltaTime, Space.Self);
+			// rotation
+#if DEBUG
+			skyboxNight.Rotate(Vector3.forward, 1f * Time.deltaTime * 2, Space.Self);
+			moonBillboard.Rotate(Vector3.forward, -0.9f * Time.deltaTime * 2, Space.Self);
+#else
+			skyboxNight.Rotate(Vector3.forward, 1f * Time.deltaTime * 0.5f, Space.Self);
+			moonBillboard.Rotate(Vector3.forward, -0.9f * Time.deltaTime * 0.5f, Space.Self);
+#endif
 
+			// movement
 			worldPos = PlayerManager.PlayerTransform.position - WorldMover.currentMove;
 			transform.position = new Vector3(worldPos.x * .001f, 0, worldPos.z * .001f);
 
+
 			Vector3 sunPos = Sun.transform.position - transform.position;
 			Sun.intensity = Mathf.Clamp01(sunPos.y);
+			Sun.color = Color.Lerp(new Color(1f, 0.5f, 0), Color.white, Sun.intensity);
 
-			StarMaterial.SetFloat("_Visibility", (-Sun.intensity + 1) * .1f);
+			StarMaterial.SetFloat("_Visibility", (-Sun.intensity + 1) * .01f);
+
+			MoonMaterial.SetFloat("_MoonDayNight", Mathf.Lerp(2.19f, 1.5f, Sun.intensity));
+			MoonMaterial.SetFloat("_Exposure", Mathf.Lerp(2f, 4f, Sun.intensity));
 
 			SkyMaterial.SetFloat("_Exposure", Mathf.Lerp(.01f, 1f, Sun.intensity));
+			SkyMaterial.SetFloat("_AtmosphereThickness", Mathf.Lerp(0.1f, 1f, Mathf.Clamp01(Sun.intensity * 10)));
 
-			CloudMaterial.SetFloat("_CloudBright", Mathf.Lerp(.02f, .9f, Sun.intensity));
+			CloudMaterial.SetFloat("_CloudBright", Mathf.Lerp(.002f, .9f, Sun.intensity));
 			CloudMaterial.SetFloat("_CloudGradient", Mathf.Lerp(.45f, .2f, Sun.intensity));
 
 			RenderSettings.fogColor = Color.Lerp(nightFog, defaultFog, Sun.intensity);
