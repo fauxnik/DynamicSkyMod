1700a531db2ae3da8da80a470d8a88328fa3ce97
Niko Fox <niko@fauxnik.me>
2020-08-08 00:37:23 -0700

    specify North & East as positive

--- Settings.cs
+++ Settings.cs
@@ -5,9 +5,9 @@ namespace ProceduralSkyMod
 {
     public class Settings : UnityModManager.ModSettings, IDrawable
     {
-        [Draw(Label = "Latitude", Min = -90, Max = 90)]
+        [Draw(Label = "Latitude (+N/-S)", Min = -90, Max = 90)]
         public float latitude = 44.7872f;
-        [Draw(Label = "Longitude", Min = -180, Max = 180)]
+        [Draw(Label = "Longitude (+E/-W)", Min = -180, Max = 180)]
         public float longitude = (float)(TimeZoneInfo.Local.GetUtcOffset(DateTime.Now).TotalHours + (TimeZoneInfo.Local.IsDaylightSavingTime(DateTime.Now) ? -1 : 0)) * 15;
 
         override public void Save(UnityModManager.ModEntry entry)
